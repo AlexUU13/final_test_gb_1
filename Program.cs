21ccd6e56601295c4c37b02e02583627f89c2fc7
Aleksey.Usikov <Atuwqwer@gmail.com>
2023-08-13 19:38:51 +0300

    creater array

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿string[] CreateNewArray(int size)
+{
+    string[] array = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+
