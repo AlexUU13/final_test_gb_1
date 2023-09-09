adad699b35cd38f724b93a00c8d1e5c96f5e56d7
Aleksey.Usikov <Atuwqwer@gmail.com>
2023-09-09 20:06:18 +0300

    ver3.2

--- Program.cs
+++ Program.cs
@@ -23,7 +23,7 @@ string[] CreateNewArray(int size)
     return array;
 }
 
-string[] Find(string[] array)
+string[] ChangeArray(string[] array)
 {
     string[] new_array = new string[array.Length];
     int j = 0;
@@ -39,23 +39,32 @@ string[] Find(string[] array)
 }
 
 
-System.Console.WriteLine("set the array by yourself (yes or no)");
+System.Console.WriteLine("set the array by yourself? (yes or no)");
 string a = Convert.ToString(Console.ReadLine());
 
 if (a == "yes")
 {
     System.Console.WriteLine("enter array size");
     int size = Convert.ToInt32(Console.ReadLine());
-    System.Console.WriteLine(String.Join(", ", Find(CreateNewArray(size))));
+    System.Console.WriteLine(String.Join(", ", ChangeArray(CreateNewArray(size))));
 
 }
-if (a == "no")
+else
 {
-    System.Console.WriteLine("enter array size");
-    int size = Convert.ToInt32(Console.ReadLine());
-    System.Console.WriteLine("enter min value");
-    int minValue = Convert.ToInt32(Console.ReadLine());
-    System.Console.WriteLine("enter max value");
-    int maxValuse = Convert.ToInt32(Console.ReadLine());
-    System.Console.WriteLine(String.Join(", ", Find(CreateRandomeArray(size, minValue, maxValuse))));
+    if (a == "no")
+    {
+        System.Console.WriteLine("enter array size");
+        int size = Convert.ToInt32(Console.ReadLine());
+        System.Console.WriteLine("enter min value");
+        int minValue = Convert.ToInt32(Console.ReadLine());
+        System.Console.WriteLine("enter max value");
+        int maxValuse = Convert.ToInt32(Console.ReadLine());
+        System.Console.WriteLine(String.Join(", ", ChangeArray(CreateRandomeArray(size, minValue, maxValuse))));
+    }
+
+    else
+    {
+        System.Console.WriteLine("you wrote the wrong value");
+    }
+
 }
